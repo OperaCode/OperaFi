678cb14e815486d3d731a12cbfdb6002b179edc3
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-03-22 22:39:45 +0100

    verified contract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,21 +5,35 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-
-
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     hardhat: {},
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
+    // sepolia: {
+    //   url: process.env.LISK_SEPOLIA_URL || "",
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    //   chainId: 11155111,
+    // },
+    liskSepolia: {
+      url: process.env.LISK_SEPOLIA_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111,
+      chainId: 4202,
     },
   },
   etherscan: {
-   apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: { liskSepolia: "anything" },
+    customChains: [
+      {
+        network: "liskSepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
   },
+  sourcify: { enabled: false },
 };
 
 export default config;
